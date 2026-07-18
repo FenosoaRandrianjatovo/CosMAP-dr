bc5a0980283d6f042ea106199059f569e96016f8
Fenosoa Randrianjatovo <98966847+FenosoaRandrianjatovo@users.noreply.github.com>
2026-07-17 22:18:30 -0400

    Bump version from 0.1.0 to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cosmap-dr",
-    version="0.1.0",
+    version="0.1.1",
     description="CosMAP: Contrastive Manifold Approximation and Projection",
     author="Fenosoa Randrianjatovo",
     packages=find_packages(where="src"),
